0736d9bea97ac653a2d693092df9a33f3b279dd2
Duẩn <91420446+Duan-Dinh@users.noreply.github.com>
2021-11-26 18:09:32 +0700

    change

--- 1.Source/FCDC/src/main/java/com/fpt/myweb/controller/StaffController.java
+++ 1.Source/FCDC/src/main/java/com/fpt/myweb/controller/StaffController.java
@@ -26,7 +26,7 @@ public class StaffController {
     @Autowired
     private UserService userService;
     @GetMapping("/allPatientForStaff")// fomat sang DTO trả về dữ liệu
-        public ResponseEntity<CommonRes> getAll(@PathParam("villageId") Long villageId,@PathParam("search") String key,@PathParam("key") String text,@PathParam("page") Integer page) {
+        public ResponseEntity<CommonRes> getAll(@PathParam("villageId") Long villageId,@PathParam("key") String key,@PathParam("text") String text,@PathParam("page") Integer page) {
         CommonRes commonRes = new CommonRes();
         try {
             commonRes.setResponseCode(ErrorCode.PROCESS_SUCCESS.getKey());
